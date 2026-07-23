f03d9b0e2385103e2c81e18a6d83aaae57ffd260
Luigichopper <luigichopper01@gmail.com>
2026-07-23 16:39:23 -0500

    Add simple lighting to root scene

--- src/core/player/player.gd
+++ src/core/player/player.gd
@@ -2,7 +2,7 @@
 extends CharacterBody2D
 
 const WALK_SPEED: float = 80.0
-const RUN_SPEED: float = 100.0
+const RUN_SPEED: float = 120.0
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
@@ -10,7 +10,7 @@ func _physics_process(_delta: float) -> void:
 	var direction := Input.get_vector("left", "right", "up", "down")
 	var speed := RUN_SPEED if Input.is_action_pressed("sprint") else WALK_SPEED
 	velocity = direction * speed
-	
+	print(velocity)
 	# Sprite flipping
 	if direction.x != 0:
 		sprite_2d.flip_h = (direction.x < 0)
